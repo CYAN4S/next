e67510502569ed21db0305c5f050a43e6af9b600
CYAN4S <me@cyan4s.com>
2024-03-31 23:36:47 +0900

    Initial commit

---
+++ Assets/Scripts/RotatingCubeSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+public partial struct RotatingCubeSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<RotateSpeed>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        /* When not using Job
+        foreach (var (localTransform, rotateSpeed)
+                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>())
+        {
+            localTransform.ValueRW =
+                localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+        }
+        */
+
+        var rotatingCubeJob = new RotatingCubeJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+        rotatingCubeJob.ScheduleParallel();
+    }
+    
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+        
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+            localTransform = localTransform.RotateY(rotateSpeed.value * deltaTime);
+        }
+    }
+}
